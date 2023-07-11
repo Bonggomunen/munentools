d274b3f75b4bf03e9adc4f4b3f26ae02b8f3edda
Bonggomunen <93695639+Bonggomunen@users.noreply.github.com>
2023-07-11 17:01:41 +0700

    Add files via upload

--- qrcodegenerator/script.js
+++ qrcodegenerator/script.js
@@ -1,24 +1,75 @@
-const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
-let preValue;
+const container = document.querySelector(".container");
+const userInput = document.getElementById("userInput");
+const submitBtn = document.getElementById("submit");
+const downloadBtn = document.getElementById("download");
+const sizeOptions = document.querySelector(".sizeOptions");
+const BGColor = document.getElementById("BGColor");
+const FGColor = document.getElementById("FGColor");
+let QR_Code;
+let sizeChoice, BGColorChoice, FGColorChoice;
 
-generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value.trim();
-    if(!qrValue || preValue === qrValue) return;
-    preValue = qrValue;
-    generateBtn.innerText = "Generating QR Code...";
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-    qrImg.addEventListener("load", () => {
-        wrapper.classList.add("active");
-        generateBtn.innerText = "Generate QR Code";
-    });
+//Set size
+sizeOptions.addEventListener("change", () => {
+  sizeChoice = sizeOptions.value;
 });
 
-qrInput.addEventListener("keyup", () => {
-    if(!qrInput.value.trim()) {
-        wrapper.classList.remove("active");
-        preValue = "";
-    }
+//Set background color
+BGColor.addEventListener("input", () => {
+  BGColorChoice = BGColor.value;
 });
+
+//Set foreground color
+FGColor.addEventListener("input", () => {
+  FGColorChoice = FGColor.value;
+});
+
+//Format input
+const inputFormatter = (value) => {
+  value = value.replace(/[^a-z0-9A-Z]+/g, "");
+  return value;
+};
+
+submitBtn.addEventListener("click", async () => {
+  container.innerHTML = "";
+  //QR code genertion
+  QR_Code = await new QRCode(container, {
+    text: userInput.value,
+    width: sizeChoice,
+    height: sizeChoice,
+    colorDark: FGColorChoice,
+    colorLight: BGColorChoice,
+  });
+
+  //Set url for download
+  const src = container.firstChild.toDataURL("image/pmg");
+  downloadBtn.href = src;
+  let userValue = userInput.value;
+  try {
+    userValue = new URL(userValue).hostname;
+  } catch (_) {
+    userValue = inputFormatter(userValue);
+    downloadBtn.download = `${userValue}QR`;
+    downloadBtn.classList.remove("hide");
+  }
+});
+
+userInput.addEventListener("input", () => {
+  if (userInput.value.trim().length < 1) {
+    submitBtn.disabled = true;
+    downloadBtn.href = "";
+    downloadBtn.classList.add("hide");
+  } else {
+    submitBtn.disabled = false;
+  }
+});
+
+window.onload = () => {
+  container.innerHTML = "";
+  sizeChoice = 100;
+  sizeOptions.value = 100;
+  userInput.value = "";
+  BGColor.vavlue = BGColorChoice = "#ffffff";
+  FGColor.value = FGColorChoice = "#377dff";
+  downloadBtn.classList.add("hide");
+  submitBtn.disabled = true;
+};
